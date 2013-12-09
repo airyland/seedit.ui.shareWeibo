60356f159e7089d7ea0c6ce61cd27ca29aa6ef81
airyland <airyland@qq.com>
2013-12-09 18:47:02 +0800

    commit unfinish work

--- src/shareWeibo.js
+++ src/shareWeibo.js
@@ -1,6 +1,18 @@
-define(function(require, exports, module) {
-
-  var shareWeibo;
-
-  module.exports = shareWeibo;
+define(function (require, exports, module) {
+    var shareWeibo = {};
+    (function ($) {
+        var API = require('seedit/API/0.0.1/API');
+        var dialog = require('seedit/dialog/0.0.1/dialog');
+        shareWeibo.open = function (options, success, error) {
+            var defaults = {
+                title: '分享到新浪微博',  // 标题
+                successCallback: $.noop,  // 成功回调
+                errorCallback: $.noop,  // 失败回调
+                content: '', // 分享内容 ,
+                placeholder: '填写分享内容哦' // 内容为空时的提示
+            };
+            $.extend(defaults, options);
+        };
+    })(jQuery);
+    module.exports = shareWeibo;
 });
