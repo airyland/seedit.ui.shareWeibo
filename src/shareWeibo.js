d507960788996ecc3fad79d4cd375965db06aeb9
李政 <airyland@qq.com>
2013-12-10 22:51:16 +0800

    add txt limit option

--- src/shareWeibo.js
+++ src/shareWeibo.js
@@ -9,7 +9,8 @@ define(function (require, exports, module) {
                 successCallback: $.noop,  // 成功回调
                 errorCallback: $.noop,  // 失败回调
                 content: '', // 分享内容 ,
-                placeholder: '填写分享内容哦' // 内容为空时的提示
+                placeholder: '填写分享内容哦', // 内容为空时的提示
+                limit:140 // 字数限制
             };
             $.extend(defaults, options);
         };
